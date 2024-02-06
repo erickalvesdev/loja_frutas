39869b520db5dd03b7bfb01f5bd28b6dd12dba49
Erick Alves <erickalves01232@gmail.com>
2024-02-06 10:11:22 -0300

    function 02

--- FUNCTION.sql
+++ FUNCTION.sql
@@ -1,3 +1,31 @@
+--FUNCTION PARA RETORNAR VALOR DO SALARIO
+CREATE FUNCTION FUNC_ValorSalarial(
+    @ID_Cargo    INT
+)
+RETURNS money
+AS
+/*
+DOCUMENTAÇÃO
+ARQUIVO FONTE.....: FUNC_ValorSalarial.SQL
+OBJETIVO..........: FUNCTION PARA RETORNAR VALOR DO SALARIO
+AUTOR.............: ERICK ALVES
+DATA..............: 05/02/2024
+EX................: SELECT [dbo].[FUNC_ValorSalarial] (@Id_Cargo)
+*/
+    BEGIN
+        DECLARE @Salario money
+
+        --VERIFICANDO SE O ID EXISTE
+        IF @ID_Cargo IS NOT NULL
+
+        SELECT @Salario = ValorSalarial
+        FROM Cargo
+        WHERE ID = @ID_Cargo
+
+        RETURN @Salario
+    END
+GO
+
 --FUNCTION PARA ATUALIZAR QUANTIDADE EM ESTOQUE
 CREATE FUNCTION FUNC_AtualizarEstoqueAposCompra(
 	@ID_Produto INT, 
