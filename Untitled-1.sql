2fc15e3799f764ac7cc49e9fa0d86ce894743458
Erick Alves <erickalves01232@gmail.com>
2024-02-06 09:45:02 -0300

    select de selecao

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,30 +1,23 @@
---FUNCTION PARA ATUALIZAR QUANTIDADE EM ESTOQUE
-CREATE FUNCTION FUNC_AtualizarEstoqueAposCompra(
-	@ID_Produto INT, 
-	@Quantidade INT
-)
-RETURNS INT
-AS
-/*
-DOCUMENTAÇÃO
-ARQUIVO FONTE.....: FUNC_AtualizarEstoqueAposCompra.SQL
-OBJETIVO..........: ATUALIZAR QUANTIDADE EM ESTOQUE
-AUTOR.............: ERICK ALVES
-DATA..............: 05/02/2024
-EX................: SELECT [dbo].[FUNC_AtualizarEstoqueAposCompra] (@Id_Produto, @Quantidade)
-*/
-	BEGIN
-		DECLARE @QuantidadeTotalEmEstoque INT,
-				@QuantidadeEmEstoque int
+-- Use PATINDEX para encontrar produtos que contenham a palavra "fruta" na descrição
+SELECT ID_Produto, Nome
+    FROM Produto
+WHERE PATINDEX('%fruta%', Nome) > 0
 
-			-- Obtenha o novo valor de estoque
-			SELECT @Quantidade = C.Quantidade, @QuantidadeEmEstoque = P.QuantidadeEmEstoque
-			FROM COMPRA C
-			INNER JOIN Produto P ON C.IdProduto = P.ID
-			WHERE C.IdProduto = @ID_Produto
+-- Use PATINDEX para encontrar produtos que contenham a palavra "fruta" no nome e SUBSTRING para extrair parte do nome
+SELECT 
+    ID_Produto, Nome
+    SUBSTRING(Nome, PATINDEX('%fruta%', nome), LEN('fruta')) AS ParteNome
+    FROM Produto
+WHERE PATINDEX('%fruta%', nome) > 0
 
-			SET @QuantidadeTotalEmEstoque = @QuantidadeEmEstoque - @Quantidade
+-- Use REPLACE para substituir a palavra "nome" por "nome2" na descrição
+SELECT 
+    ID_Produto, Nome 
+    REPLACE(Nome, 'Abacaxi', 'Abacate') AS NovaoNome
+FROM Produto
 
-			RETURN @QuantidadeTotalEmEstoque
-	END
-GO
+-- Use REVERSE para inverter a descrição dos produtos
+SELECT 
+    ID_Produto, Nome
+    REVERSE(Nome) AS NomeInvertido
+FROM Produto
